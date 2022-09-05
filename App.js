09f092cd2df2c72475ceb81c98b663bc6fcb7bda
13nikita1999 <98888822+13nikita1999@users.noreply.github.com>
2022-09-05 13:39:03 +0530

    Apparel Assignment React upload sucessfully

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import './App.css'
+
+const App = () => { 
+  return (
+    <>
+       <div class="container">
+        <div class="box left-box">
+            <div class="navbar">
+                <span class="dot"></span>
+                <h3>BASE <br /> apparel</h3>
+            </div>
+        </div>
+        <div class="box mid-box"></div>
+        <div class="box right-box">
+            <div class="main-section">
+                <h1 class="head1">we're</h1>
+                <h1 class="head2">coming <br />soon</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa in possimus accusamus suscipit mollitia nesciunt quia laboriosam hic et maiores at modi quasi assumenda blanditiis repellat </p>
+            </div>
+            <div class="footer">
+                <form action="">
+                    <input type="email" placeholder="Email Address" id="text" />
+                    <button type="submit" >C</button>
+                </form>
+            
+        </div>
+        </div>
+        
+    </div>
+    </>
+  )
+}
+
+export default App
